6dd750bbb49400ec48a1a806bf929b18b5b7d716
CAMR05 <cmoheno@centro.edu.mx>
2025-09-30 22:23:22 -0600

    se intenta corregir la animación del atardecer utilizando timeline

--- S03/atardecer/app.js
+++ S03/atardecer/app.js
@@ -4,25 +4,25 @@ console.log(gsap);
 
 
 window.addEventListener("mousedown", function () {
-        gsap.to(
+    const tl = gsap.timeline();
+        tl.to(
             ".sol",
             {
                 y:-500,
                 x:innerWidth/2 - 100,
                 
-                duration:2, //segundos
+                duration:3, //segundos
                 ease:"power2.inOut",
-                onComplete:function() {
-                    gsap.to(
+                }
+        );
+                    tl.to(
                         ".sol",
                         {
                             x:1500,
                             y:80,
-                            duration:2,
-                            ease:"power2.in",
-                        }
-                    )
-                }
-            }
-        );
+                            duration:3,
+                            ease:"power2.inOut",
+                        },
+                   "-=0.7" );
+                
 });
